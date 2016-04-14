a6e5c837327c07d7d5e2842fd8e7f92f5789f54d
Kevin Terrone <kevint97@hotmail.it>
2016-04-14 12:46:09 +0200

    adding some propriety of the navbar

--- Home_Page_Admin.php
+++ Home_Page_Admin.php
@@ -1,41 +1,62 @@
 <html>
  <head>
   <title>GolFinder</title>
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <script src="bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
  </head>
  <body>
-<?php
+<div class="container-fluid">
+  <ul class="nav nav-pills" data-spy="affix" style="background-color: #ffffff">
+    <li class="active"><a data-toggle="pill" href="#home">Home</a></li>
+    <li><a data-toggle="pill" href="#squadra">Inserisci Squadra</a></li>
+    <li><a data-toggle="pill" href="#notizie">Inserisci Notizia</a></li>
+    <li><a data-toggle="pill" href="#scontro">Inserisci Scontro</a></li>
+    <li><a data-toggle="pill" href="#registrazione">Inserisci Registrazione</a></li>
+  </ul>
+  
+  <div class="tab-content" style="margin-top: 50px;">
+    <div id="home" class="tab-pane fade in active">
+      <h3>HOME</h3>
+      <p>Benvenuto nel lato amministratore</p>
+    </div>
+    <div id="squadra" class="tab-pane fade">
+      <h3>Inserisci Squadra</h3>
+      <form action='Insert_Team.php' method='POST' align=center> 
+          <h4 align=center>Nome:</p> 
+          <input  type='text' name='nome' /> 
+          <h4 align=center>Posizione:</p> 
+          <input  type='text' name='posizione' />
+          <h4 align=center>Punti:</p> 
+          <input  type='text' name='punti' />
+          <h4 align=center>Campionato:</p> 
+          <input  type='text' name='campionato' />
+          <h4 align=center>Nazione:</p> 
+          <input  type='text' name='nazione' />
+          <h4 align=center>Ultima Partita:</p> <br>
+          <input  type='radio' name='ult_partita' value=TRUE/>Vinta 
+          <input  type='radio' name='ult_partita' value=FALSE/>Persa<br><br>
+          <h4 align=center>Quotazione:</p> 
+          <input  type='text' name='quotazione' /> <br> <br>
+		  <input type='submit' value="Inserisci" align=center>
+      </form>
+    </div>
+    <div id="notizie" class="tab-pane fade">
+      <h3>Inserisci Notizia</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+    <div id="scontro" class="tab-pane fade">
+      <h3>Inserisci Scontro</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+    <div id="registrazione" class="tab-pane fade">
+      <h3>Registra Amministratore</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+  </div>
+</div>
 
-echo("<ul class='nav nav-pills'>
-	   <li class='active'>
-	   <a data-toggle='pill' href='#home'>Home</a></li>
-	   <li><a data-toggle='pill' href='#squadre'>Inserisci Squadre</a></li>
-	   <li><a data-toggle='pill' href='#scontri'>Inserisci Scontro</a></li>
-	   <li><a data-toggle='pill' href='#notizie'>Inserisci Notizie</a></li>
-	  </ul>
-
-	  <div class='tab-content'>
-	    <div id='home' class='tab-pane fade in active'>
-	      "//Inserire in questi campi i vari form
-	      "
-	    </div>
-	    <div id='squadre' class='tab-pane fade in active'>
-	      "//Inserire in questi campi i vari form
-	      "
-	    </div>
-	    <div id='scontri' class='tab-pane fade in active'>
-	      "//Inserire in questi campi i vari form
-	      "
-	    </div>
-	    <div id='notizie' class='tab-pane fade in active'>
-	      "//Inserire in questi campi i vari form
-	      "
-	    </div>
-	  </div>");
-
-
-	
-
-?>
 </body>
 </html>
---
+++ Insert_Team.php
@@ -0,0 +1,57 @@
+<?php
+
+
+$servername = "localhost";
+$username = "root";
+$password = "kappiere97";
+$conn = new mysqli($servername, $username, $password);
+if ($conn->connection_error){
+	die("Connection failed: " . $conn->connection_error);
+}
+$Query_Instruction="
+                    SELECT COUNT(*) AS'conto'
+                    FROM gol.squadra
+                    WHERE squadra.id IN(
+                                        SELECT id
+                                        FROM gol.squadra
+                                        WHERE nome='" . $_POST['nome'] . "' OR
+                                              (posizione='" . $_POST['posizione'] . "' AND
+                                               serie='" . $_POST['campionato'] . "'));";
+$x=$conn->query($Query_Instruction);
+$y=mysqli_fetch_array($x);
+
+	if($y['conto']>0){
+		echo("<h3>La squadra esiste o i dati sono stati inseriti in precedenza</h3>");
+		Header("refresh:4;Home_Page_Admin.php");
+	}
+    if($y['conto']==0){
+	
+		$nome=$_POST['nome'];
+		$posizione=$_POST['posizione'];
+		$punti=$_POST['punti'];
+		$campionato=$_POST['campionato'];
+		$nazione=$_POST['nazione'];
+		$ult_partita=$_POST['ult_partita'];
+		$quotazione=$_POST['quotazione'];
+
+		$Query_Instruction="
+		                    INSERT INTO gol.squadra VALUES(
+		                    '',
+		                    '$nome',
+		                    $posizione,
+		                    $punti,
+		                    '$campionato',
+		                    '$nazione',
+		                    $ult_partita,
+		                    $quotazione);";
+		if($conn->query($Query_Instruction)===TRUE){
+            echo("ciao");
+		}
+		else{echo("fail");
+	    }
+		//Header("location:Home_Page_Admin.php");
+     
+	
+	
+    }
+?>
